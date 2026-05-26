9368f4d69773a2f4e5bc9cab57eaf767757101a0
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-05-26 15:32:19 +0800

    Shell script created

---
+++ Timestamp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "HellO"
+T1=$(date)
+echo " Script execution started - $T1"
+StartTime=$(date)
+sleep 10s
+echo "Script execution completed!- $T1"
+EndTime=$(date)
+TimeTaken=($(EndTime)-$(StartTime))
