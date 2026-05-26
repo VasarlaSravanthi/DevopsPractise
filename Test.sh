3e258ece0a4bd0a44653bdcdfcb03b14920faccc
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-05-26 14:33:25 +0800

    Shell script created

---
+++ Test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+P1=Tom 
+P2=Jack
+P3=Jhonny
+echo " $P1: Hello Moon Stars are smiling"
+echo " $P2: yes good to see"
+echo " $P3: Moon should have been more brighter"
+echo " $P1: yess yess $P1 & $P3"
