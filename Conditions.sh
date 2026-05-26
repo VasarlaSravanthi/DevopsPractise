b7ad678c0ceaa92e39a7c485e05921cd048d95ec
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-05-26 17:15:42 +0800

    Shell script created

---
+++ Conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter Number N:"
+read N
+echo "Enter Number M:"
+read M
+SUM=$N+$M
+echo "sum is $SUM"
+Movies=("KGF" "RRR" "AMB")
+if [$SUM -gt 100]; then
+    echo "First Movie is ${Movies[0]}"
+else
+echo "All Movies = ${Movies[@]}"
+fi
