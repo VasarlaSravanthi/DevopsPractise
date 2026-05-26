8d4e12086764aaed36198be33b031c6acc541b34
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-05-26 17:11:15 +0800

    Shell script created

--- DataTypes.sh
+++ DataTypes.sh
@@ -6,10 +6,10 @@ read N2
 SUM=$(($N+$N2))
 echo "sum is $SUM"
 Movies=("KGF" "RRR" "AMB")
-if ($SUM>100){
+if [$SUM -ge 100]; then
+
     echo "First Movie is ${Movies[0]}"
-}
+
 else
-{
 echo "All Movies = ${Movies[@]}"
-}
+fi
