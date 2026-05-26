485504b60f508a97dab6009b4c88de04034db25b
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-05-26 14:52:25 +0800

    Shell script created

--- Test.sh
+++ Test.sh
@@ -8,3 +8,8 @@ echo " $P1: Hello Moon Stars are smiling"
 echo " $P2: yes good to see"
 echo " $P3: Moon should have been more brighter"
 echo " $P1: yess yess $P1 & $P3"
+
+S1=$1
+S2=$2
+echo "$P1 what are you doing"
+echo "$P2 I am working"
