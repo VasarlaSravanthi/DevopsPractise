d4757ceb1b0323d6246fd1e8dfb4c11802f79994
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-05-26 18:02:53 +0800

    Shell script created

--- InstallScript.sh
+++ InstallScript.sh
@@ -5,4 +5,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
  echo " Root User is not activated"
 
+ Read N
+  if [ $N -eq 1 ]; then
+  sudo su
+  echo " Root user is Activated"
+  else
+  exit 1
+
 fi
