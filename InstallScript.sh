13a68f1e2ff9e8a5668ff4f06f868ec757a55b58
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-06-02 15:24:37 +0800

    Practise

--- InstallScript.sh
+++ InstallScript.sh
@@ -4,17 +4,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
  echo " Root User is not activated"
+ exit 1
+fi
+sudo su
+echo " Root user is Activated"
+echo " Installation of MYSQL started....."
+dnf install mysql -y
+if [ $? -eq 0 ]; then
+   echo " Installing MYSQL is Failed.."
+   exit 1
 else
-   
-   echo " Root user is Activated"
-   echo " Installation of MYSQL started....."
-   dnf install mysql -y
-   if [ $? -eq 0 ]; then
-      echo " Installing MYSQL is Failed.."
-      exit 1
-    else
-      echo "Intsalling MYSQL is Success!!!"
-    fi
+   echo "Intsalling MYSQL is Success!!!"
+fi   
 
    
-fi
