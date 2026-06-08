b8a44764e1274af5ad905e7f91e9f3b2f41fdcd3
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-06-08 17:42:43 +0800

    shellscript

--- Shell-Roboshop/roboshop/mongo.sh
+++ Shell-Roboshop/roboshop/mongo.sh
@@ -33,8 +33,16 @@ Validate()
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-Validate $? "Adding Mongo repo"
+VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y &>> LOG_FILE
-Validate $? "Installing Mongodb"
+dnf install mongodb-org -y &>> $LOGS_FILE
+VALIDATE $? "Installing MongoDB"
 
+systemctl enable --now mongod
+VALIDATE $? "Starting and enabling MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB"
