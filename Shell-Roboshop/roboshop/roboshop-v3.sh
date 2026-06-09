774e273b1cae0175db0f65363d062e74ab07dab6
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-06-09 21:46:03 +0800

    testing

--- Shell-Roboshop/roboshop/roboshop-v3.sh
+++ Shell-Roboshop/roboshop/roboshop-v3.sh
@@ -41,7 +41,7 @@ fi
 
 get_instance_id()
 {
-    name=$2
+    name=$instance
     aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
 }
 
