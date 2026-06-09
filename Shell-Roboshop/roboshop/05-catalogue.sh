dc9aab3820f76f57577bbd727614eb76f953b11c
VasarlaSravanthi <sravanthivsf@gmail.com>
2026-06-09 13:56:19 +0800

    testing

--- Shell-Roboshop/roboshop/05-catalogue.sh
+++ Shell-Roboshop/roboshop/05-catalogue.sh
@@ -69,7 +69,7 @@ VALIDATE $? "Installed MongoDB client"
 
 INDEX=$(mongosh --host mongodb.shop --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
-if (($INDEX -lt 0)) ; then
+if [ $INDEX -lt 0 ]; then
     mongosh --host mongodb.shop </app/db/master-data.js &>>$LOGS_FILE
     VALIDATE $? "Load Products"
 else
